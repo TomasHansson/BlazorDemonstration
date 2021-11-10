829b942998ef608112ea1a59648fe61c606ee31c
Tomas Hansson <tomas.hansson@afry.com>
2021-11-10 11:27:22 +0100

    Add project files.

---
+++ RazorClassLibrary/Components/JavaScriptComponent.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Components
+{
+    public partial class JavaScriptComponent : IAsyncDisposable
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+        
+        private string message;
+        private IJSObjectReference jsModule;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/RazorClassLibrary/Components/JavaScriptComponent.razor.js");
+            }
+        }
+
+        public async void ShowPrompt()
+        {
+            if (jsModule != null)
+            {
+                var result = await jsModule.InvokeAsync<string>("showPrompt", "Awesome Title from Blazor");
+                if (!string.IsNullOrWhiteSpace(result))
+                {
+                    message = result;
+                    StateHasChanged();
+                }
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (jsModule != null)
+            {
+                await jsModule.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ RazorClassLibrary/Services/CounterStateService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Services
+{
+    public class CounterStateService
+    {
+        private int currentCount;
+        public int CurrentCount
+        {
+            get { return currentCount; }
+            set { currentCount = value; CurrentCountChanged?.Invoke(); }
+        }
+        public Action CurrentCountChanged { get; set; }
+    }
+}
