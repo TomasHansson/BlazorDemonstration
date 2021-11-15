63ffc1b90c23365f396d685bcd9fc0d11427d5be
Tomas Hansson <tomas.hansson@afry.com>
2021-11-15 10:05:38 +0100

    Add example of basic form and validations.

---
+++ RazorClassLibrary/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Models
+{
+    public class Person
+    {
+        [Required]
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        [Range(0,150)]
+        public int Age { get; set; }
+    }
+}
