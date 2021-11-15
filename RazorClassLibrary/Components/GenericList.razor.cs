b5ac8eec5ad769adc304855ee41c9d6fe7f7aa28
Tomas Hansson <tomas.hansson@afry.com>
2021-11-15 11:11:45 +0100

    Add basic example of a generic component.

---
+++ RazorClassLibrary/Components/GenericList.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Components
+{
+    public partial class GenericList<T>
+    {
+        [Parameter]
+        public List<T> Items { get; set; }
+        private List<PropertyInfo> properties;
+
+        protected override void OnInitialized()
+        {
+            properties = typeof(T).GetProperties().ToList();
+        }
+    }
+}
---
+++ RazorClassLibrary/Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary.Models
+{
+    public class Character
+    {
+        public string Hero { get; set; }
+        public string Archetype { get; set; }
+        public string Class { get; set; }
+        public string Subclass { get; set; }
+    }
+}
